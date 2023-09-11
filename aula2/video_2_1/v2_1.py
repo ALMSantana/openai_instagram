c06bcd1f58baa9dd6f458e0ad08cded1c5b08f2e
ALMSantana <andrelms4@insper.edu.br>
2023-09-11 09:54:34 -0300

    Cortando - conteudo novo

--- aula2/video_2_1/v2_1.py
+++ aula2/video_2_1/v2_1.py
@@ -2,24 +2,6 @@ import openai
 from dotenv import load_dotenv
 import os
 
-def openai_whisper_transcrever(caminho_audio, nome_arquivo, modelo_whisper, openai):
-    print("Estou transcrevendo com o whispers ...")
-
-    audio = open(caminho_audio, "rb")
-
-    resposta = openai.Audio.transcribe(
-        api_key=openai.api_key,
-        model = modelo_whisper,
-        file = audio
-    )
-
-    transcricao = resposta.text
-
-    with open(f"texto_completo_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
-        arquivo_texto.write(transcricao)
-
-    return transcricao
-
 def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai):
     print("Resumindo com o gpt para um post do instagram ...")
 
@@ -57,20 +39,3 @@ def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai):
         arquivo_texto.write(resumo_instagram)
 
     return resumo_instagram
-
-def main():
-    load_dotenv()
-
-    caminho_audio = "podcasts/hipsters_154_testes_short.mp3"
-    nome_arquivo = "hipsters_154_testes_short"
-    url_podcast = "https://www.hipsters.tech/testes-de-software-e-inteligencia-artificial-hipsters-154/"
-    
-    api_openai = os.getenv("API_KEY_OPENAI")
-    openai.api_key = api_openai
-
-    modelo_whisper = "whisper-1"
-    transcricao_completa = openai_whisper_transcrever(caminho_audio,nome_arquivo,modelo_whisper,openai)
-    resumo_instagram = openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai)
-
-if __name__ == "__main__":
-    main()  
